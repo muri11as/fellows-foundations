50fe192a1d18a6add111523016ae14a522f6659f
cmurillas <cesar.murillas@mlb.com>
2020-09-02 21:22:35 -0400

    revision to counter 1

--- counter.py
+++ counter.py
@@ -1,17 +1,15 @@
 print ("Please enter the number of coins:")
-print ("#of quarters:")
-quarters = input()
-print ("#of dimes:")
-dimes = input()
-print ("#of nickels"
-nickels = input()
-print ("#ofpennies")
-pennies = input()
+
+int quarters = input("#of quarters:")
 quartertot = quarters * .25
+int dimes = input("#of dimes:")
 dimestot = dimes * .10
-nickeltot = nickels* .05
+int nickels = input("#of nickels:")
+nickeltot = nickels * .05
+int pennies = input("#ofpennies:")
 pennytot = pennies *.01
-tot = quartertot + dimestot+nickeltot+pennytot
+
+tot = quartertot + dimestot + nickeltot + pennytot
 cents = tot % 1
 dollars = tot // 1
 print ("The total is ",dollars , "dollars and ", cents,"cents")
