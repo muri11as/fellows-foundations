4eb01388236a6c14abbdbda19d0d9ccd183ef000
cmurillas <cesar.murillas@mlb.com>
2020-09-02 23:38:53 -0400

    fix coins

--- coins.py
+++ coins.py
@@ -1,19 +1,25 @@
 print("Enter dollar and cent amount:")
 amt = float(input())
-if (amt => .25):
-	quarters = amt//.25
-	amt = amt - (quarters*.25)
+quarters = 0
+dimes = 0
+nicks = 0
+pens = 0
+if (amt >= .25):
+	quarters = int(amt//.25)
+amt = amt - (quarters*.25)
 amt = round(amt%1,2)
-elif (amt => .10):
-	dimes = amt // .10
-	amt = amt - (dimes*.10)
+if (amt >= .10):
+	
+	dimes = int(amt // .10)
+amt = amt - (dimes*.10)
 amt = round(amt%1,2)
-elif(amt => .05 and amt < .10):
-	nicks = amt //.05
-	amt = amt -(nickels*.05)
+if(amt >= .05 and amt < .10):
+	
+	nicks = int(amt //.05)
+amt = amt -(nicks*.05)
 amt = round(amt%1,2)
-else:
-	pens = amt//.01
+if (amt>.01 and amt <.05):
+	pens = int(amt//.01)
 
 
-print("The coins are",quarters,"quarters,",dimes,"dimes,",nickels,"nickels and",pens,"pennies")
+print("The coins are",quarters,"quarters,",dimes,"dimes,",nicks,"nickels and",pens,"pennies")
