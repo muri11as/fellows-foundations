bb7bf01c5e92b83cc3314e0b488b71afb54ca445
cmurillas <cesar.murillas@mlb.com>
2020-09-02 23:24:00 -0400

    coins2

--- coins.py
+++ coins.py
@@ -1,14 +1,14 @@
 print("Enter dollar and cent amount:")
 amt = float(input())
-if amt >= .25:
+if (amt => .25):
 	quarters = amt//.25
 	amt = amt - (quarters*.25)
 amt = round(amt%1,2)
-elif amt >= .10:
+elif (amt => .10):
 	dimes = amt // .10
 	amt = amt - (dimes*.10)
 amt = round(amt%1,2)
-elif(amt >= .05 and amt < .10):
+elif(amt => .05 and amt < .10):
 	nicks = amt //.05
 	amt = amt -(nickels*.05)
 amt = round(amt%1,2)
