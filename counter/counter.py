eb21a28060f786e14df63aab758e146d6e2f6cf5
cmurillas <cesar.murillas@mlb.com>
2020-09-02 21:43:58 -0400

    add folders

---
+++ counter/counter.py
@@ -0,0 +1,14 @@
+print ("Please enter the number of coins:")
+quarters = int(input("#of quarters:"))
+quartertot = quarters * .25
+dimes = int(input("#of dimes:"))
+dimestot = dimes * .10
+nickels = int(input("#of nickels:"))
+nickeltot = nickels * .05
+pennies = int(input("#ofpennies:"))
+pennytot = pennies *.01
+
+tot = float(quartertot + dimestot + nickeltot + pennytot)
+cents = tot%1
+dollars = int(tot//1)
+print ("The total is ",dollars , "dollars and ", cents,"cents")
