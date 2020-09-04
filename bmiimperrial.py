ed75a22b7105824a1bcfd6d3ac21fce2f3c976b3
cmurillas <cesar.murillas@mlb.com>
2020-09-03 23:36:37 -0400

    addimperial

---
+++ bmiimperrial.py
@@ -0,0 +1,12 @@
+pounds = float(input())
+height = float(input())
+
+lbkg = .043592
+hmeter = .0254
+
+lbs = pounds * lbkg
+meters = height * hmeter
+
+bmi = lbs/(meters**2)
+
+print ("BMI is:",bmi)
