13ec357a5bbb69996587bf96c6de5bf49bdc1cf9
cmurillas <cesar.murillas@mlb.com>
2020-09-04 00:01:36 -0400

    readd

---
+++ bmimetric2.py
@@ -0,0 +1,23 @@
+pounds = float(input())
+height = float(input())
+
+lbkg = .453592
+hmeter = .0254
+
+#lbs = pounds * lbkg
+#meters = height * hmeter
+status="N/A"
+
+#bmi = lbs/(meters**2)
+bmi = pounds/(height**2)
+
+if bmi < 18.5:
+	status = "Underweight"
+elif 18.5 <= bmi < 25:
+	status =  "Normal"
+elif 25 <= bmi < 30:
+	status = "Overweight"
+else:
+	status = "Obese"
+
+print ("BMI is:",bmi, "Status is",status)
