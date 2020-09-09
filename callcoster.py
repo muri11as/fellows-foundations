8d9e713ffc3eab59bd4d0fccf5fe9968d2a125ea
cmurillas <cesar.murillas@mlb.com>
2020-09-08 23:06:53 -0400

    callcollector add

---
+++ callcoster.py
@@ -0,0 +1,23 @@
+day = input()
+dlist = ["Mon","Tue", "Wed","Thr", "Fri", "Sat", "Sun"]
+weekday  = 0
+weekend = 0
+if day in dlist[:5]:
+	weekday = 1
+else:
+	weekend = 1
+rate = 0
+timeStart = int(input())
+if ((weekday == 1 )and (timeStart < 800 or timeStart > 1800)):
+	rate = .25
+elif ((weekday == 1 )and ( 800 <= timeStart <= 1800)):
+	rate = .40
+else:
+	rate =.15
+	
+dur = int(input())
+
+cost = dur * rate
+
+print("This call will cost $ %.2f" % cost)
+
