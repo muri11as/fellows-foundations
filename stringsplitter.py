45c6b5940a9a30ed8796df8ebe604574224f6016
cmurillas <cesar.murillas@mlb.com>
2020-09-08 23:59:47 -0400

    add splitty1

--- stringsplitter.py
+++ stringsplitter.py
@@ -3,4 +3,4 @@ stringy = input()
 mididx = len(stringy)//2
 print ("Middle character:", stringy[mididx])
 print ("First half:", stringy[:mididx])
-print ("First half:", stringy[mididx:])
+print ("second half:", stringy[mididx:])
