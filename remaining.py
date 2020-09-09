e218389ae39c6cd49088d6acb3440826b0d07818
cmurillas <cesar.murillas@mlb.com>
2020-09-09 01:00:17 -0400

    add remain

---
+++ remaining.py
@@ -0,0 +1,13 @@
+def remaining(string):
+	stringer = " "
+	stringy = string.split(' ')
+	#print (stringy[1:])
+	stringer = stringer.join(stringy[1:])
+	return (stringer)
+# def main():
+# 	s = input()
+# 	print(remaining(s))
+# 	
+# main()
+
+		
