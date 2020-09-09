16f820288e286956645a4c4e55445ac2bb177c73
cmurillas <cesar.murillas@mlb.com>
2020-09-08 22:43:26 -0400

    cashreg edit

--- cashregister.py
+++ cashregister.py
@@ -1,5 +1,11 @@
 item1 = input()
 item2 = input()
+base = float(item1) + float(item2)
+if item1 > item2:
+	item2 = float(item2) * .5
+else:
+	item1 = float(item1) * .5
+
 cardIn = input ()
 if cardIn == 'Y' or cardIn == 'y':
 	card = 1
@@ -7,10 +13,8 @@ else:
 	card = 0
 tax = input()
 
+print("Base price = %.2f"% origbase)
 base = float(item1) + float(item2)
-
-#base = (base,2)
-print("Base price = %.2f"% base)
 if card == 1:
 	disc = base * .1
 else:
@@ -18,7 +22,7 @@ else:
 disc = base - disc
 print("Price after discounts = %.2f"% disc)
 tax = float(tax)/100
-tot = disc - (disc * tax)
+tot = disc + (disc * tax)
 
 print("Total price = %.2f" %tot)
 
