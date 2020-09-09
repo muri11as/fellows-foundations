2062270d8ec2c4ba6ccf42f5c292717d5163f511
cmurillas <cesar.murillas@mlb.com>
2020-09-09 01:19:51 -0400

    change idx

--- maxabsinlst.py
+++ maxabsinlst.py
@@ -1,6 +1,6 @@
 def max_abs_val(lst):
 	maxy = 0
-	for i in range(0,len(lst)-1):
+	for i in range(0,len(lst)):
 		abby = abs(lst[i])
 		if abby >= maxy:
 			maxy = abby
--- maxinlst.py
+++ maxinlst.py
@@ -1,6 +1,6 @@
 def max_val(lst):
 	maxy = lst[0]
-	for i in range(0,len(lst)-1):
+	for i in range(0,len(lst)):
 		if lst[i] >= maxy:
 			maxy = lst[i]
 	return maxy
