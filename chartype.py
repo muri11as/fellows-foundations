0ae900a440588737c5883cc99ac5361b35909b84
cmurillas <cesar.murillas@mlb.com>
2020-09-09 00:16:32 -0400

    char edit

--- chartype.py
+++ chartype.py
@@ -1,9 +1,9 @@
 charmander = input()
 
 if type(charmander) == str and charmander.islower():
-	print (charmander, "is a lowercase letter.")
+	print (charmander, "is a lower case letter.")
 elif type(charmander) == str and charmander.isupper():
-	print (charmander, "is an uppercase letter.")
+	print (charmander, "is an upper case letter.")
 elif type(charmander) == str and charmander.isdigit():
 	print (charmander, "is a digit.")
 else:
